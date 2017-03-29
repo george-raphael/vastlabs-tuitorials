20a8c1141623fb32a015b2cb0e15495bad509410
georgie817 <georgie817@gmail.com>
2017-03-29 17:04:20 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+
+	<title>Learning PHP</title>
+
+
+</head>
+<body>
+	<h1>loaded</h1>
+
+
+	<?php
+
+	$name =  "Fed";
+
+		echo 'My name is $name';
+
+		ECHO "Hello World3!<br>";
+		echo "Hello World1!<br>";
+		EcHo "Hello World2!<br>";
+
+	 ?>
+</body>
+</html>
